3664daf8038d1123941d0f7c46c250ed7f4f0a4f
Jose Ivanov Ramirez Tirado <ramireztiradojose@gmail.com>
2021-02-24 22:23:26 -0800

    index routes updated v1.1

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
 
 
 router.use('/especies', require('./especies'));
+router.use('/animales', require('./animales'));
+router.use('/empleados', require('./empleados'));
+router.use('/usuarios', require('./usuarios'));
+router.use('/zonas', require('./zonas'));
 
 /* con el método use de nuestro router estamos indicando 
 * que en la ruta 'v1/usuarios' estarán anidadas las rutas 
