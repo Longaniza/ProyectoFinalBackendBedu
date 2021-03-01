6144a781557c21e8e4a406a0f86a7e4076a0d220
Ivanov <ramireztiradojose@gmail.com>
2021-02-28 22:53:53 -0800

    Primera version estructura general completa,comentarios extras eliminados

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,5 @@ router.use('/observaciones', require('./observaciones'));
 router.use('/zonas', require('./zonas'));
 router.use('/revisiones', require('./revisiones'));
 
-/* con el método use de nuestro router estamos indicando 
-* que en la ruta 'v1/usuarios' estarán anidadas las rutas 
-* que vamos a crear en nuestro archivo usuarios.js,
-* la función require está importando este archivo */
 
 module.exports = router;
