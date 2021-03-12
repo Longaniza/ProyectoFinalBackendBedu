2d8c5ed4ac356637b74c21526e2624ddeced20b8
Ivanov <ramireztiradojose@gmail.com>
2021-03-11 21:11:16 -0800

    Extra Files deleted

--- clasespostwork3/Revision.js
+++
@@ -1,9 +0,0 @@
-class Revision {
-    constructor(id, idEmpleado, idZona, descripcion, fecha) {
-        this.id = id;
-        this.idEmpleado = idEmpleado;
-        this.idZona = idZona;
-        this.descripcion = descripcion;
-        this.fecha = fecha;
-    }
-}
